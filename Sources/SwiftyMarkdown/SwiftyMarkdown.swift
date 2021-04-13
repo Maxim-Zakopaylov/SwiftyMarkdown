a027b382bf4732a575f17e07db2bfe5a6b346570
Maxim Zakopaylov <maxim.zakopaylov@gmail.com>
2021-04-13 13:56:48 +0300

    fix image url

--- Sources/SwiftyMarkdown/SwiftyMarkdown.swift
+++ Sources/SwiftyMarkdown/SwiftyMarkdown.swift
@@ -593,11 +593,6 @@ extension SwiftyMarkdown {
 				}
 				#if !os(macOS)
 				let image1Attachment = NSTextAttachment()
-				image1Attachment.image = UIImage(named: token.metadataStrings[imgIdx])
-				let str = NSAttributedString(attachment: image1Attachment)
-				finalAttributedString.append(str)
-				#elseif !os(watchOS)
-				let image1Attachment = NSTextAttachment()
                 if let urlImage = URL(string: token.metadataStrings[imgIdx]) {
                     if let data = try? Data(contentsOf: urlImage) {
                         image1Attachment.image = NSImage(data: data)
@@ -605,10 +600,15 @@ extension SwiftyMarkdown {
                         finalAttributedString.append(str)
                     }
                 } else {
-                    image1Attachment.image = NSImage(named: token.metadataStrings[imgIdx])
+                    image1Attachment.image = UIImage(named: token.metadataStrings[imgIdx])
                     let str = NSAttributedString(attachment: image1Attachment)
                     finalAttributedString.append(str)
                 }
+				#elseif !os(watchOS)
+				let image1Attachment = NSTextAttachment()
+                image1Attachment.image = NSImage(named: token.metadataStrings[imgIdx])
+                let str = NSAttributedString(attachment: image1Attachment)
+                finalAttributedString.append(str)
 				#endif
 				continue
 			}
